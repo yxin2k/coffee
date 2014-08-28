c9ff79c8cb85d7c2e9429e76218b1f7302dc847c
yxin2k <broli@Yuchaos-MacBook-Pro.local>
2014-08-28 00:10:08 -0400

    membership app scaffold

---
+++ coffeesite/memberships/models/__init__.py
@@ -0,0 +1,1 @@
+from memberships.models.userprofile import UserProfile
---
+++ coffeesite/memberships/models/userprofile.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+
+class Meta:
+    app_label = 'memberships'
