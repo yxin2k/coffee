3d2f4305b9fa5d88215fb0e46fdfa5021a5bfc1d
yxin2k <broli@Yuchaos-MacBook-Pro.local>
2014-08-28 21:36:09 -0400

    enabled admin for editing user data

--- coffeesite/memberships/admin.py
+++ coffeesite/memberships/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from memberships.models import UserProfile
+from memberships.models import Province
+from memberships.models import Country
+from memberships.models import SubscriptionType
+from memberships.models import GrindType
 
 admin.site.register(UserProfile)
+admin.site.register(Province)
+admin.site.register(Country)
+admin.site.register(SubscriptionType)
+admin.site.register(GrindType)
